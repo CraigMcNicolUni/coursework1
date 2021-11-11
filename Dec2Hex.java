d1f65ccefe0024832f9a5a3ce0df407044b9b845
CraigMcNicolUni <cmcnic202@caledonian.ac.uk>
2021-11-11 21:32:24 +0000

    added all error handling

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -6,38 +6,34 @@ class Dec2Hex
     public static int Arg1;
 
     public static void main(String args[])
-    {
-	if ( Arg1 == (int)Arg1)
-	{ 
- 	try{
-		Arg1 = Integer.parseInt(args[0]);
-	}
-	catch (NumberFormatException nfe){
-		System.out.println("Enter a number");
-		System.exit(0);
-	}
+        {
+                if ( args.length == 0 ){
+                System.out.println("there is no value");
+                System.exit(0);
+                } else{
+                        try{
+                            Integer.parseInt(args[0]);
+                        } catch(NumberFormatException e) {
+                            System.out.println("This isnt an Integer");
+                            System.exit(0);}
+                        
+                        Arg1 = Integer.parseInt(args[0]);
+                        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+                        int rem, num;
+                        num = Arg1;
+                        {
+                                String hexadecimal="";
+                                System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
-        Arg1 = Integer.parseInt(args[0]);
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem, num;
-        num = Arg1;
-	{
-        	String hexadecimal="";
-        	System.out.println("Converting the Decimal Value " + num + " to Hex...");
+                                while(num != 0)
+                                {
+                                        rem=num%16;
+                                        hexadecimal= ch[rem] + hexadecimal;
+                                        num= num/16;
+                                }
 
-        	while(num != 0)
-        	{	
-            		rem=num%16;
-            		hexadecimal= ch[rem] + hexadecimal;
-            		num= num/16;
-        	}	
+                                System.out.println("Hexadecimal representation is: " + hexadecimal);
+                        }
+        } 
+        }}
 
-        	System.out.println("Hexadecimal representation is: " + hexadecimal);
-	}
-    	} else {
-	       System.out.println("ERROR!! integer is required");
-       		System.exit(0);
-	}		
-
-}
-}
